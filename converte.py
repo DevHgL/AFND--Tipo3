c4b45816bcb79523df293f2ca44a9c69ee124264
Felipe Alves <felipeam@id.uff.br>
2023-10-22 18:35:30 -0300

    menu interativo

--- converte.py
+++ converte.py
@@ -2,21 +2,23 @@ import sys
 from funcoesAuxiliares import limpaTela, filtra, escreve
 
 def leAutomato(nomeArquivo):
-    if not ".txt" in nomeArquivo[-4::]:
+    if not ".txt" in nomeArquivo[-4:]:
         nomeArquivo += ".txt"
     arquivo = open(nomeArquivo, 'r')
     automato = arquivo.readlines()
     arquivo.close()
-    return filtra(automato)
+    return filtra(automato) # Retorna o automato ja sem quebra de linha, espaços, etc
 
-def trocaInicio(gramatica, I):
+def trocaInicio(gramatica, novoInicio):
+    # Substitui o símbolo de início 1 pelo novo símbolo
     aux = []
     for linha in gramatica:
-        linha = linha.replace("1", "0").replace(I, "1").replace("0", I)
+        linha = linha.replace("1", "0").replace(novoInicio, "1").replace("0", novoInicio)
         aux.append(linha)
     return aux
 
 def converteSimbolos(gramatica):
+    # Converte os símbolos numéricos em letras ou "S" de acordo com as regras
     aux = []
     for linha in gramatica:
         auxLinha = ""
@@ -32,16 +34,15 @@ def converteSimbolos(gramatica):
 
 def fazConversao(automato):
     gramatica = []
-    aux = automato[0]
-    inicio = aux[-1]
-    aux = automato[1]
-    finais = aux[3::2]
+    # Obtém o símbolo de início e os estados finais do autômato
+    inicio = automato[0][-1]
+    finais = automato[1][3::2]
 
     for f in finais:
         linha = f + " -> ε"
         gramatica.append(linha)
 
-    for transicao in automato[2::]:
+    for transicao in automato[2:]:
         simbolo = transicao[1]
         caractere = transicao[3]
         for c in transicao[7::2]:
@@ -52,6 +53,7 @@ def fazConversao(automato):
             gramatica.append(linha)
 
     if inicio != "1":
+        # Se o símbolo de início não for 1, troca-o
         gramatica = trocaInicio(gramatica, inicio)
 
     gramatica = sorted(gramatica)
@@ -60,17 +62,24 @@ def fazConversao(automato):
     return gramatica
 
 def main():
-    nomeArquivo = input("Digite o nome do arquivo: ")
+    num = 9
+    while True:
+        nomeArquivo = input("Digite o nome do arquivo: ")
 
-    automato = leAutomato(nomeArquivo)
-    print("\nAFND:")
-    escreve(automato)
+        automato = leAutomato(nomeArquivo)
+        print("\nAFND:")
+        escreve(automato)
 
-    gramatica = fazConversao(automato)
-    print("\nGramatica:")
-    escreve(gramatica)
+        gramatica = fazConversao(automato)
+        print("\nGramatica:")
+        escreve(gramatica)
 
-    input("Pressione ENTER para continuar . . .")
+        num = input("Digite 1 caso deseje continuar e 0 caso deseje parar: ")
+        limpaTela()
+        if (num == "0"):
+            break
+
+    print("Encerrando Programa!")
 
 if __name__ == "__main__":
     main()
